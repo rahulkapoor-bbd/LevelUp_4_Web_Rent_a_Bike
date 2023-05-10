701c838a676dba4da87082763383a2899fc545c8
Oarabile Moima <oarabile@bbd.co.za>
2023-05-10 15:15:38 +0200

    Dummy data and new owner constrainnt on bike table

--- DatabaseScripts.sql
+++ DatabaseScripts.sql
@@ -11,6 +11,7 @@ CREATE TABLE Bike (
     bikeId INT IDENTITY(1,1) PRIMARY KEY CLUSTERED,
     typeId INT,
     statusId INT,
+    userId INT,
     dailyRate MONEY
 );
 
@@ -51,6 +52,11 @@ ADD CONSTRAINT FK_Bike_BikeType
 FOREIGN KEY (typeId)
 REFERENCES BikeType(typeId);
 
+ALTER TABLE Bike
+ADD CONSTRAINT FK_Bike_BikeOwner
+FOREIGN KEY (userId)
+REFERENCES BikeType(userId);
+
 ALTER TABLE Rentals
 ADD CONSTRAINT FK_Rentals_Bike
 FOREIGN KEY(bikeId)
@@ -64,9 +70,34 @@ REFERENCES Users(userId);
 
 -- sample data
 
+-- Insert BikeType data
+INSERT INTO BikeType (description) VALUES ('Mountain Bike');
+INSERT INTO BikeType (description) VALUES ('Road Bike');
+INSERT INTO BikeType (description) VALUES ('Hybrid Bike');
+INSERT INTO BikeType (description) VALUES ('Gravel Bike');
 
+-- Insert BikeStatus data
+INSERT INTO BikeStatus (description) VALUES ('Available');
+INSERT INTO BikeStatus (description) VALUES ('Rented');
+INSERT INTO BikeStatus (description) VALUES ('Under Maintenance');
 
+-- Insert Users data
+INSERT INTO Users (firstname, lastname, emailAddress) VALUES ('John', 'Doe', 'johndoe@gmail.com');
+INSERT INTO Users (firstname, lastname, emailAddress) VALUES ('Jane', 'Doe', 'janedoe@yahoo.com');
+INSERT INTO Users (firstname, lastname, emailAddress) VALUES ('Bob', 'Smith', 'bobsmith@hotmail.com');
 
+-- Insert Bike data
+INSERT INTO Bike (typeId, statusId, dailyRate, userId) VALUES (1, 1, 25.00, 1);
+INSERT INTO Bike (typeId, statusId, dailyRate, userId) VALUES (1, 2, 25.00, 2);
+INSERT INTO Bike (typeId, statusId, dailyRate, userId) VALUES (2, 1, 30.00, 1);
+INSERT INTO Bike (typeId, statusId, dailyRate, userId) VALUES (2, 3, 30.00, 3);
+INSERT INTO Bike (typeId, statusId, dailyRate, userId) VALUES (3, 1, 20.00, 2);
+
+-- Insert Rentals data
+INSERT INTO Rentals (bikeId, userId, rentalStart, rentalEnd) VALUES (1, 1, '2023-05-01', '2023-05-03');
+INSERT INTO Rentals (bikeId, userId, rentalStart, rentalEnd) VALUES (2, 2, '2023-05-02', '2023-05-04');
+INSERT INTO Rentals (bikeId, userId, rentalStart, rentalEnd) VALUES (3, 1, '2023-05-05', '2023-05-08');
+INSERT INTO Rentals (bikeId, userId, rentalStart, rentalEnd) VALUES (4, 3, '2023-05-06', '2023-05-09');
 
 
 
