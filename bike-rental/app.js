cf0ae611206f48a00db12d529d67890549d997c2
rahulkapoor-bbd <122476067+rahulkapoor-bbd@users.noreply.github.com>
2023-05-17 10:22:32 +0200

    Attempt of tedious for db

--- bike-rental/app.js
+++ bike-rental/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const db = require('./dbconnection/db');
 var path = require('path');
 
-db.connect();
+db.connect()
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/user');
--- bike-rental/dbconnection/db.js
+++ bike-rental/dbconnection/db.js
@@ -1,29 +1,65 @@
-const sql = require('mssql');
+// const sql = require('mssql');
+const { Connection, Request } = require('tedious');
+
+// const config = {
+//   server: "localhost",
+//   database: "BikeRental",
+//   options: {
+//     trustedConnection: true
+//   }
+// };
 
 const config = {
-  server: "localhost",
-  database: "BikeRental",
+  authentication: {
+    type: 'default',
+    options: {
+      userName: '',
+      password: ''
+    }
+  },
+  server: '127.0.0.1', 
   options: {
-    trustedConnection: true
+    database: 'BikeRental',
+    encrypt: false
   }
 };
 
-/*
-async function connect() {
-  try {
-    await sql.connect(config);
-    console.log('Connected to database');
-  } catch (err) {
-    console.error('Error connecting to database: ', err);
+
+const connection = new Connection(config);
+
+connection.on('connect', err => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+  } else {
+    console.log('Connected to the database.');
+
+    // Execute your queries here
+    executeQuery();
   }
-}
-*/
-async function connect() {
-  console.log('connected to db');
+});
+
+connection.on('error', err => {
+  console.error('Database error:', err);
+});
+
+function executeQuery() {
+  const request = new Request('SELECT * FROM Bikes', (err, rowCount, rows) => {
+    if (err) {
+      console.error('Error executing query:', err);
+    } else {
+      console.log('Query result:', rows);
+    }
+
+    connection.close();
+  });
+
+  connection.execSql(request);
 }
 
+function connect() {
+  connection.connect()
+}
 
 module.exports = {
-  connect: connect,
-  sql: sql
+  connect: connect
 };
