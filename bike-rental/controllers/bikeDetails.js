517aaa405fe63d5ed1b017da7c851feee599f558
Stephen Potgieter <stephenp@bbd.co.za>
2023-05-17 16:04:25 +0100

    added get get bike by id and get description by typeid backend functions

--- bike-rental/controllers/bikeDetails.js
+++ bike-rental/controllers/bikeDetails.js
@@ -1,1 +1,34 @@
 const router = require('express').Router();
+const pool = require('../dbconnection/db')
+
+const getBikeDetails = (bikeId) => {
+    return new Promise((resolve, reject) => {
+      pool.query('SELECT * FROM bike WHERE bikeId = ?', [bikeId], (error, results) => {
+        if (error) {
+          console.error('Error executing query:', error);
+          reject(error);
+          return;
+        }
+        
+        if (results.length === 0) {
+          // Bike with the specified ID was not found
+          resolve(null);
+          return;
+        }
+        
+        const bike = {
+          bikeId: results[0].bikeId,
+          typeId: results[0].typeId,
+          statusId: results[0].statusId,
+          userId: results[0].userId,
+          dailyRate: results[0].dailyRate
+        };
+  
+        resolve(bike);
+      });
+    });
+  };
+
+  module.exports = {
+    getBikeDetails
+  }
--- bike-rental/routes/bikeRental.js
+++ bike-rental/routes/bikeRental.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const bikeRentalController = require('../controllers/bikeRental');
+const utilsController = require('../controllers/ulilities');
 
   router.get('/', async (req, res, next) => {
   try {
     const bikeInfo = await bikeRentalController.getBikeInfo();
-    res.render('bikeRental', { data: bikeInfo });
+    const description = await utilsController.getDescriptionFromBikeType(bikeInfo.typeId)
+    console.log(description)
+    res.render('bikeRental', { data: bikeInfo, description: description });
   } catch (err) {
     next(err);
   }
