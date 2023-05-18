755c392a3345d1b82094660f5dd26ac7722db631
Cameron <122345679+camerond-bbd@users.noreply.github.com>
2023-05-18 14:01:13 +0200

    Created and fixed API calls
    - The call to insert a rental was created and works on Postman
    - The Bike Details page API call was changed and fixed. All relative information is retrieved through one SQL statement.
    - Utilities was removed as that function was replaced and is not needed anymore.

--- bike-rental/routes/bikeDetails.js
+++ bike-rental/routes/bikeDetails.js
@@ -1,32 +1,38 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 const bikeDetailsController = require('../controllers/bikeDetailsController');
-const utilsController = require('../controllers/ulilities');
+//const rentalController = require('../controllers/rentalController');
 
-let selectedBike = {
+/*let selectedBike = {
     bikeId: 1,
     bikeName: 'Roadster',
     bikeType: 'Road Bike',
     dailyRate: '$6',
     availability: true
-  }
+  }*/
 
 
 router.get('/', async (req, res, next) => {
   try {
-    // going to need another query to get the description of the bikeType, for the specific bike
-    // the getDescriptionFromBikeId needs to accept the bike id as well
     const bikeId = req.query.bikeId;
-    console.log("selected bike id: "+bikeId);
     const bike = await bikeDetailsController.getBikeDetails(bikeId);
-    const description = await utilsController.getDescriptionFromBikeType(bike.typeId);
-    console.log(description);
-    res.render('bikeDetails', { data: bike, description: description });
+    res.render('bikeDetails', { data: bike });
   } catch (err) {
     next(err);
   }
 });
 
+router.post('/', async (req, res, next) => {
+  try {
+    var bikeId = req.body.bikeId;
+    var userId = req.body.userId;
+    var rentalStart = req.body.rentalStart;
+    var rentalEnd = req.body.rentalEnd;
 
+    const rentalInfo = await bikeDetailsController.createNewRental(bikeId, userId, rentalStart, rentalEnd);
+    res.status(201).send({ message: `Successfully created rental booking` });
+  } catch (err) {
+    next(err)
+  }
+});
 
 module.exports = router;
