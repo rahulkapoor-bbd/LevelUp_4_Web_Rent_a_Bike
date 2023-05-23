479938ee9422466484a8b71cca989732cf832549
Cameron <122345679+camerond-bbd@users.noreply.github.com>
2023-05-23 17:24:19 +0200

    Update db.js AGAIN

--- bike-rental/dbconnection/db.js
+++ bike-rental/dbconnection/db.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: 'bikedb.cph9smjdgcof.af-south-1.rds.amazonaws.com',
   user: 'admin',
   password: 'RentABike',
-  database: 'bikerental'
+  database: 'BikeRental'
 });
 
 module.exports = pool;
