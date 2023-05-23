7102cfb49f7e44e911c9bb59f372fa2a39424a34
rahulkapoor-bbd <122476067+rahulkapoor-bbd@users.noreply.github.com>
2023-05-23 08:57:33 +0200

    Added login [age

--- bike-rental/app.js
+++ bike-rental/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 const db = require('./dbconnection/db');
 var path = require('path');
 const pool = require('./dbconnection/db');
@@ -7,6 +9,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/user');
 const bikeRentalRouter = require('./routes/bikeRental');
 const bikeDetailsRouter = require('./routes/bikeDetails');
+const authRouter = require('./routes/userAuth');
 
 const app = express();
 app.use(express.json());
@@ -14,14 +17,36 @@ app.use(express.json());
 // allow to server static files from this directory
 app.use(express.static('public'));
 
+// Session middleware
+app.use(
+  session({
+    secret: 'your_secret_key', // Change this to a random secret key
+    resave: false,
+    saveUninitialized: true
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Middleware to check if the user is authenticated
+const requireAuth = (req, res, next) => {
+  if (req.session.isAuthenticated) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+};
+
 // view engine setup and pages
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use('/', authRouter);
+app.use('/login', authRouter); // Login route does not require authentication
+app.use('/', requireAuth, authRouter); // Protect index route with authentication
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
 app.use('/bikeRental', bikeRentalRouter);
 app.use('/bikeDetails', bikeDetailsRouter);
-
 
 app.listen(3000, () => {
   console.log('Listening on '+ 3000);
